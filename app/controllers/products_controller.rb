71bb9bea91757f15bd3cb39a2cefcbdcf1394f74
Kyle Gundry <Gundr1kt@gmail.com>
2017-03-12 10:32:14 -0500

    Added new and create routes with some bootstrap css

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,23 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     render "index.html.erb"
+  end 
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      description: params[:description]
+      )
+
+
+    product.save
+    render "create.html.erb"
   end
 
   def show
@@ -11,4 +28,5 @@ class ProductsController < ApplicationController
 #   @recipe = Recipe.find_by(id: params[:id]) - another way to show the above.
     render "show.html.erb"
   end
+
 end
