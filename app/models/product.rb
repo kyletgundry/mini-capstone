d9b923a99dfeb3bb8ddccb3e48307728969395bd
Kyle Gundry <Gundr1kt@gmail.com>
2017-03-15 19:43:42 -0500

    Add styling and Model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,25 @@
 class Product < ApplicationRecord
+  def sale_message
+    if price.to_i < 25
+      return "Discount Item!!!"
+    else
+      return "Everyday Value!!"
+    end
+  end
+
+  def tax
+    price.to_i * 0.09
+  end
+
+  def total
+    tax + price
+  end
+
+  def discounted?
+    if price.to_f < 25
+      return true
+    else
+      return false
+    end
+  end
 end
